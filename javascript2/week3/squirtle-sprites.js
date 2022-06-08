3970e4dfbeeb202804da0165060d326f20740208
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2022-06-08 12:06:14 +0200

    Update squirtle-sprites.js

--- javascript2/week3/squirtle-sprites.js
+++ javascript2/week3/squirtle-sprites.js
@@ -15,7 +15,7 @@ const parent = document.querySelector("body")
 const myDiv = document.createElement("div")
 
 const heading1 = document.createElement("h1")
-heading1.innerText = "Pokemon Game"
+heading1.innerText = "Pokemon Turning Game"
 
 const heading2 = document.createElement("h2")
 heading2.innerText = pokemonData.name.toUpperCase()
