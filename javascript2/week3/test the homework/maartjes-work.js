85486bfbc0ec96d7bd154df1b1ffe012beb646f3
Priya Pradhan <102596886+Priya-Pdh@users.noreply.github.com>
2022-06-08 16:05:31 +0200

    Create maartjes-work.js

---
+++ javascript2/week3/test the homework/maartjes-work.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const monday = [
+  {
+    name: 'Write a summary HTML/CSS',
+    duration: 180,
+  },
+  {
+    name: 'Some web development',
+    duration: 120,
+  },
+  {
+    name: 'Fix homework for class10',
+    duration: 20,
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200,
+  },
+];
+
+const tuesday = [
+  {
+    name: 'Keep writing summary',
+    duration: 240,
+  },
+  {
+    name: 'Some more web development',
+    duration: 180,
+  },
+  {
+    name: 'Staring out the window',
+    duration: 10,
+  },
+  {
+    name: 'Talk to a lot of people',
+    duration: 200,
+  },
+  {
+    name: 'Look at application assignments new students',
+    duration: 40,
+  },
+];
+
+const maartjesTasks = monday.concat(tuesday);
+const maartjesHourlyRate = 20;
+
+function computeEarnings(tasks, hourlyRate) {
+  // Replace this comment and the next line with your code
+  const totalEarning = tasks
+    .filter(workDurations => workDurations.duration > 119)
+    .map(workDurations => (workDurations.duration / 60) * hourlyRate)
+    .reduce((sum, arr) => sum + arr, 0);
+  return totalEarning;
+}
+
+// eslint-disable-next-line no-unused-vars
+const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
+
+// add code to convert `earnings` to a string rounded to two decimals (euro cents)
+const earningWithDecimals = earnings.toFixed(2);
+
+console.log(`Maartje has earned €${earningWithDecimals}`);
+
+// Do not change or remove anything below this line
+module.exports = {
+  maartjesTasks,
+  maartjesHourlyRate,
+  computeEarnings,
+};
